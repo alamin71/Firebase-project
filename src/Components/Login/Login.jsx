9cc3d3ae57423ad8c46cd47c4cd3b967cd66736e
AL-AMIN <alamin50cse.miu@gmail.com>
2024-05-22 00:48:59 +0600

    Enable Github Sign In And Create Github Application

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
 import { useState } from "react";
 
@@ -6,10 +6,11 @@ const Login = () => {
     const [user, setUser] = useState(null)
     const auth = getAuth(app);
     //console.log(app)
-    const provider = new (GoogleAuthProvider);
+    const googleProvider = new (GoogleAuthProvider);
+    const gitHubProvider =new (GithubAuthProvider);
 
     const handleGoogleSignIn = () => {
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
             .then(result => {
                 const LoggedInUser = result.user;
                 setUser(LoggedInUser);
@@ -25,9 +26,20 @@ const Login = () => {
                 console.log(result)
                 setUser(null)
             })
-            .cath(error => {
+            .catch(error => {
                 console.log('error', error.message)
             })
+    }
+    const handleGitHubSignIN =()=>{
+        signInWithPopup(auth, gitHubProvider)
+        .then(result =>{
+           const LoggedInUser =result.user;
+            console.log(LoggedInUser)
+            setUser(null)
+        })
+        .catch(error =>
+            console.log('error',error.message)
+        )
     }
 
 
@@ -35,7 +47,11 @@ const Login = () => {
         <div>
             {user ?
                 <button onClick={handleSignOut}>SignOut</button> :
+                <>
                 <button onClick={handleGoogleSignIn}>Google login</button>
+                <button onClick={handleGitHubSignIN}>GitHub Login</button>
+                
+                </>
             }
             {user && <div>
                 <h1>User: {user.displayName}</h1>
